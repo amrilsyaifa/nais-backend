81dd9de543093b77c1f601873ca65bbd55a65406
amrils <amril.yasin@kpiinstitute.org>
2021-11-02 16:40:57 +0700

    update

---
+++ src/server/RootServer.ts
@@ -0,0 +1,50 @@
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import http from 'http';
+
+// Schema
+import AuthenticationSchema from '../graphql/schemas/AuthenticationSchema';
+import UserSchema from '../graphql/schemas/UserSchema';
+import RoleSchema from '../graphql/schemas/RoleSchema';
+import PermissionSchema from '../graphql/schemas/PermissionSchema';
+import ProfileSchema from '../graphql/schemas/ProfileSchema';
+
+// Resolver
+import AuthResolver from '../graphql/resolvers/Authentications/AuthenticationResolver';
+import UsersResolver from '../graphql/resolvers/Users/UsersResolver';
+import RoleResolver from '../graphql/resolvers/Roles/RoleResolver';
+import PermissionsResolver from '../graphql/resolvers/Permissions/PermissionsResolver';
+import ProfilesResolver from '../graphql/resolvers/Profiles/ProfilesResolver';
+
+// Utils
+import Authentication from '../utils/Authentication';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const StartServer = async (App: any, PORT: string | number): Promise<void> => {
+    const app = new App().app;
+
+    const httpServer = http.createServer(app);
+
+    // Graphql
+    const server = new ApolloServer({
+        typeDefs: [AuthenticationSchema, UserSchema, ProfileSchema, RoleSchema, PermissionSchema],
+        resolvers: [AuthResolver, UsersResolver, ProfilesResolver, RoleResolver, PermissionsResolver],
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+        context: async ({ req }) => {
+            const tokenWithBearer = req.headers.authorization || '';
+            const token = tokenWithBearer.split(' ')[1];
+            const user = await Authentication.getUser(token);
+            return { user };
+        }
+    });
+
+    // Start
+    await server.start();
+    server.applyMiddleware({ app });
+
+    await new Promise((resolve: any) => httpServer.listen({ port: PORT }, resolve(true)));
+    // eslint-disable-next-line no-console
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+export default StartServer
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,2 +1,3 @@
+export { default as RootServer } from './RootServer';
 export { default as WhatsappServer } from './WhatsappServer';
 export { default as SyncDatabase } from '../databases';
